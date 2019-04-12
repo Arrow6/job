41db56fcdedd506af4412831646fbc2665126dbb
Arrow6 <48833849+Arrow6@users.noreply.github.com>
2019-04-12 16:51:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var time=1;
+var tc="";
+var dtc="";
+
+function clicked(){
+    //standar code
+    var line=document.createElement("br");
+    document.body.appendChild(line);
+
+    //changeable code
+    if(time==1){
+        tc="A.Note        v1.0 (Java)";
+        dtc="A.Note v1.0.jar"
+    }else if(time==2){
+        tc="A.Note        v1.1 (Java)";
+        dtc="A.Note v1.1.jar"
+    }
+    var text=document.createElement("a");
+    text.download=dtc;
+    text.onclick="clicked()";
+    text.textContent=tc;
+    document.body.appendChild(text);
+    time++;
+}
